dd26dd98d392e2525601b9964c5bec73a24322dd
ABDUL MATEEN NANDGADI <130289266+MATEENNANDGADI@users.noreply.github.com>
2026-09-11 18:09:32 +0530

    09-11-2026 .cpp Easy 0001-two-sum 0 ms (100%) 14.9 MB (44.61%) Array

--- C++/Easy/0001-two-sum/0001-two-sum.cpp
+++ C++/Easy/0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,14 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::unordered_map<int, int> numMap;
-        
-        for (int i = 0; i < nums.size(); i++) {
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashTable;
+        for (int i =0;i<nums.size();i++){
             int complement = target - nums[i];
-            
-            if (numMap.find(complement) != numMap.end()) {
-                return {numMap[complement], i};
+            if(hashTable.count(complement)){
+            return {hashTable[complement], i};
             }
-            
-            numMap[nums[i]] = i;
+            hashTable[nums[i]] = i;
         }
-        
-        return {-1, -1};
+    return {};
     }
 };
-
-// mateen
