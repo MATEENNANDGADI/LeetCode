ee1a6ff4b05f4fc3e51c82156202c1776dbd5c14
ABDUL MATEEN NANDGADI <130289266+MATEENNANDGADI@users.noreply.github.com>
2026-09-25 20:49:21 +0530

    09-25-2026 .cpp Medium 0011-container-with-most-water 0 ms (100%) 63 MB (48.6%) Array

--- C++/Medium/0011-container-with-most-water/0011-container-with-most-water.cpp
+++ C++/Medium/0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
+    long long maxArea(vector<int>& height) {
+        long long maxwater=0;
         int i=0;
         int j=height.size()-1;
-        int max=0;
-
+        
         while(i<j){
-            int cwidth= j - i;
-            int cheight= min(height[i],height[j]);
-            int carea= cwidth*cheight;
-            if (carea>max){
-                max=carea;
-            }
-
-            if(height[i]>height[j]){
-                j--;
-            }
-            else{
-                i++;
-            }
+            int ht= min(height[i],height[j]);
+            int width= j-i;
+            long long currvol= ht*width;
+            maxwater=max(currvol,maxwater);
+            height[i]<height[j]?i++:j--;
             
         }
-        return max;
-        }
-        
+        return maxwater;
+    }
 };
